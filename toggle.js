f129d50a95b8f0eef6869015bcfe0e8b3a62fdc8
mike <matillman95@gmail.com>
2023-01-31 14:05:55 -0700

    refixed

--- toggle.js
+++ toggle.js
@@ -2,9 +2,9 @@
 
 const toggle = () => {
   var x = document.getElementById("myLinks");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
