0d3c0e527a1f43e38006a450a6720223c539778f
mike <matillman95@gmail.com>
2023-01-31 13:48:29 -0700

    hamburger bar would not go inactive

--- toggle.js
+++ toggle.js
@@ -1,17 +1,10 @@
 
-// let mainNav = document.getElementById('js-menu');
-// let navBarToggle = document.getElementById('js-navbar-toggle');
-
-// navBarToggle.addEventListener('click', function () {
-//   mainNav.classList.toggle('active');
-// });
-
 
 const toggle = () => {
   var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
